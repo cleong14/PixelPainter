ea822c9ad472bd63e734b25bcb32088fc2905a1d
Chaz Leong <cleong14@gmail.com>
2016-02-17 19:56:59 -1000

    working on using jade to render pages. able to still paint and save to mongodb. /paintings will render a page with a list of painting ids that are saved in mongodb. next step is to get saved painting to render

--- server.js
+++ server.js
@@ -5,39 +5,55 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/pixel-painter');
 
-var drawingSchema = mongoose.Schema({ painting: Object });
-var Drawing = mongoose.model('Drawing', drawingSchema);
+var paintingSchema = mongoose.Schema({ painting: Object });
+var Painting = mongoose.model('Painting', paintingSchema);
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
 
+app.set('view engine', 'jade');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('canvas');
 });
 
 app.post('/save', function (req, res) {
-  var newDrawing = new Drawing({ painting: req.body.painting });
-  newDrawing.save(function () {
+  var newPainting = new Painting({ painting: req.body.painting });
+  newPainting.save(function () {
     res.send('yay');
   });
 });
 
-app.get('/drawings/:id', function(req, res) {
 
+app.get('/paintings/:id', function(req, res) {
+  var id = req.params.id;
+
+  Painting.findOne({ _id: id })
+  .then(function (result) {
+    res.render(result);
+  });
 });
 
-app.post('/drawings', function (req, res) {
-  var newDrawing = new Drawing({name: req.body.name});
-  newDrawing.save();
+app.get('/paintings', function (req, res) {
+  Painting.find()
+  .then(function (paintings) {
+    res.render('listing', { paintings: paintings });
+  });
+});
+
+app.post('/paintings', function (req, res) {
+  var newPainting = new Painting({name: req.body.name});
+  newPainting.save();
   res.send('Painting saved!');
 });
 
-app.put('/drawings/:id', function (req, res) {
+app.put('/paintings/:id', function (req, res) {
 
 });
 
@@ -50,3 +66,5 @@ db.once('open', function () {
     console.log('Listening to port', server.address().port);
   });
 });
+
+module.exports = app;
